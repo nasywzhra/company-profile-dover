b7f0695fc78a1335aa94a2b2c2c6e400b9f9c29a
nasywa zahra nailah <nasywazahranailah@gmail.com>
2023-12-03 22:52:33 +0700

    feat: about

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -20,43 +20,41 @@
 </head>
 
 <body>
-    <header>
-
-
-        <nav class="navbar navbar-expand-lg bg-white px-sm-5 sticky-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('page.main') }}">
-                    <img src="{{ asset('assets/img/dover-chemical.png') }}" id="navbar-logo" alt="Logo dover">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item px-3">
-                            <a class="nav-link text-dark" aria-current="page" href="{{ route('page.main') }}">HOME</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link text-dark" aria-current="page" href="#services">SERVICES</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link text-dark" aria-current="page" href="#">ABOUT</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link text-dark" aria-current="page" href="#">PRODUCT</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link text-dark" aria-current="page" href="#">CONTACT</a>
-                        </li>
-                        <li class="nav-item px-3">
-                            <a class="nav-link text-dark" aria-current="page" href="#">NEWS</a>
-                        </li>
-                </div>
+    <nav class="navbar navbar-expand-lg bg-white px-sm-5 fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('page.main') }}">
+                <img src="{{ asset('assets/img/dover-chemical.png') }}" id="navbar-logo" alt="Logo dover">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item px-3">
+                        <a class="nav-link text-dark" aria-current="page" href="{{ route('page.main') }}">HOME</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link text-dark" aria-current="page" href="#services">SERVICES</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link text-dark" aria-current="page" href="#about">ABOUT</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link text-dark" aria-current="page" href="#">PRODUCT</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link text-dark" aria-current="page" href="#">CONTACT</a>
+                    </li>
+                    <li class="nav-item px-3">
+                        <a class="nav-link text-dark" aria-current="page" href="#">NEWS</a>
+                    </li>
             </div>
-        </nav>
+        </div>
+    </nav>
 
+    <header>
         <!-- Jumbotron -->
         <div class="container-fluid bg-image px-0 object-fit-cover" id="hero">
             <img class="image-fluid" src="{{ asset('assets/img/bg-jumbotron.png') }}" alt="bg jumbotron" width="100%">
@@ -80,25 +78,59 @@
             </div>
             <div class="row justify-content-center">
                 <div class="card px-0" style="width: 310px; margin-bottom: 57px">
-                    <img src="{{ asset('assets/img/services/services-1.png') }}" class="card-img-top" alt="...">
+                    <img src="{{ asset('assets/img/services/services-1.png') }}" class="card-img-top" alt="services1">
                     <div class="card-body">
                         <h5 class="card-title text-center">Tank Storage Rental</h5>
-                        <p class="card-text text-center">We provide internal floating roof tanks for chemical storage, Other available services are heating, cooling, nitrogen blanketing, dry air, drum filling, and internal/external storage facilities.</p>
+                        <p class="card-text text-center">We provide internal floating roof tanks for chemical storage,
+                            Other available services are heating, cooling, nitrogen blanketing, dry air, drum filling,
+                            and internal/external storage facilities.</p>
                     </div>
                 </div>
                 <div class="card px-0 mx-5" style="width: 310px; margin:0px 40px 57px">
-                    <img src="{{ asset('assets/img/services/services-2.png') }}" class="card-img-top" alt="...">
+                    <img src="{{ asset('assets/img/services/services-2.png') }}" class="card-img-top" alt="services2">
                     <div class="card-body">
                         <h5 class="card-title text-center">Logistic and Trucking</h5>
-                        <p class="card-text text-center">we provide a comprehensive logistic solution for our customer. Using innovative technology and highly trained staffs, we continuously improve our delivery and warehouse performance.</p>
+                        <p class="card-text text-center">we provide a comprehensive logistic solution for our customer.
+                            Using innovative technology and highly trained staffs, we continuously improve our delivery
+                            and warehouse performance.</p>
                     </div>
                 </div>
                 <div class="card px-0 " style="width: 310px; margin-bottom: 57px">
-                    <img src="{{ asset('assets/img/services/services-3.png') }}" class="card-img-top" alt="...">
+                    <img src="{{ asset('assets/img/services/services-3.png') }}" class="card-img-top" alt="services3">
                     <div class="card-body">
                         <h5 class="card-title text-center">Drum / IBC Filling Solution</h5>
-                        <p class="card-text text-center">We have the facilities to carry out all the operations relating to drumming/IBCs.</p>
+                        <p class="card-text text-center">We have the facilities to carry out all the operations relating
+                            to drumming/IBCs.</p>
                     </div>
+                </div>
+            </div>
+        </section>
+        <section id="about" class="container-fluid px-0">
+            <div class="bg-about">
+                <img src="{{ asset('assets/img/about/about.png') }}" alt="About" class="img-fluid">
+                <div class="about-content">
+                    <h1 class="section-title">About</h1>
+                    <div class="title-line"></div>
+                    <p>Founded in 1960, The Dovechem Group is one of the leading chemical company group in Southeast
+                        Asia engaged in the business of Distribution, Manufacturing, Terminal and Logistics for the
+                        chemical industry.</p>
+                    <p>PT. Dover Chemical, located in Merak, Banten, Indonesia has been established as an Indonesia PMA
+                        Chemical Company under Dovechem Group since 1980.</p>
+                    <p>PT. Dover Chemical produces paraformaldehyde, formaldehyde, formaldehyde resins (UF, MF, PF, PUF)
+                        used in the wood panel industries and household furniture. It also produces emulsion adhesives
+                        which are utilized widely in many industries. In PT. Dover Chemical, we have been continuously
+                        improving our manufacturing process and capacity to achieve a greater production efficiency.</p>
+                </div>
+            </div>
+            <div class=" d-flex justify-content-center footer-about">
+                <div class="col-footer-about">
+                    <h6 class="footer-about-title">1960</h6>
+                    <p class="mb-0 footer-about-text">Year Establish</p>
+                </div>
+                <div class="col-line"></div>
+                <div class="col-footer-about">
+                    <h6 class="footer-about-title">4</h6>
+                    <p class="mb-0 footer-about-text">Awards Won</p>
                 </div>
             </div>
         </section>
