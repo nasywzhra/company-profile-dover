66b9ec0597b637d0b910e6880e72409f4d46e00a
nasywa zahra nailah <nasywazahranailah@gmail.com>
2023-12-06 22:04:54 +0700

    feat: product

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -26,8 +26,8 @@
                 <img src="{{ asset('assets/img/dover-chemical.png') }}" id="navbar-logo" alt="Logo dover">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -42,7 +42,7 @@
                         <a class="nav-link text-dark" aria-current="page" href="#about">ABOUT</a>
                     </li>
                     <li class="nav-item px-3">
-                        <a class="nav-link text-dark" aria-current="page" href="#">PRODUCT</a>
+                        <a class="nav-link text-dark" aria-current="page" href="#product">PRODUCT</a>
                     </li>
                     <li class="nav-item px-3">
                         <a class="nav-link text-dark" aria-current="page" href="#">CONTACT</a>
@@ -132,6 +132,77 @@
                     <h6 class="footer-about-title">4</h6>
                     <p class="mb-0 footer-about-text">Awards Won</p>
                 </div>
+            </div>
+        </section>
+
+        <section id="product" class="container-fluid">
+            <div class="row justify-content-center">
+                <h1 class="text-center section-title">
+                    PRODUCTS
+                </h1>
+                <div class="title-line"></div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="product-detail shadow">
+                    <img src="{{ asset('assets/img/product/product-1.png') }}" class="product-image" alt="product1">
+                    <div class="product-description ">
+                        <p class="product-title">
+                            Polycarboxylate Superplasticizier (PCE)
+                        </p>
+                        <div class="product-text">
+                            'Polycarboxylate Superplasticizer (PCE) is a water-soluble concrete additive used to reduce
+                            water to cement ratio without affecting the workability of the mixture.'
+                        </div>
+                    </div>
+                </div>
+                <div class="product-detail shadow">
+                    <img src="{{ asset('assets/img/product/product-2.png') }}" class="product-image" alt="product2">
+                    <div class="product-description">
+                        <p class="product-title">
+                            Sodium Benzoate - Dovative Ben
+                        </p>
+                        <p class="product-text">
+                            ‘Sodium benzoate , also know as benzoic acid sodium salt is amde by reacting sodium
+                            hydroxide with benzoic acid, is primarily used as additive and preservative in food and
+                            beverage application to reduce or inhibit activity of microorganism’
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="product-detail shadow">
+                    <img src="{{ asset('assets/img/product/product-3.png') }}" class="product-image" alt="product3">
+                    <div class="product-description">
+                        <p class="product-title">
+                            Formalydehye
+                        </p>
+                        <p class="product-text">
+                            'Formaldehyde solutions are used as raw material in a broad range of industries and
+                            applications including moulded articles, high-pressure laminates, adhesives for plywood,
+                            particleboard and other types of wood panels, coatings, varnishes and enamels, foundry core
+                            and shell binders, glass and mineral wool insulation and fertilizers.'
+                        </p>
+                    </div>
+                </div>
+                <div class="product-detail shadow">
+                    <img src="{{ asset('assets/img/product/product-4.png') }}" class="product-image" alt="product4">
+                    <div class="product-description">
+                        <p class="product-title">
+                            Paraformaldehyde
+                        </p>
+                        <p class="product-text">
+                            ' Paraformaldehyde is the solid form of liquid formaldehyde, formed by the polymerization of
+                            formaldehyde with a typical degree of polymerization of 8-100 units. '
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center" style="margin-top: 120px">
+                <img src="{{ asset('assets/img/product/logo-dover.png') }}" class="product-footer-img" alt="Logo Dover">
+                <img src="{{ asset('assets/img/product/sgs-logo.png') }}" class="product-footer-img" alt="Logo Dover">
+                <img src="{{ asset('assets/img/product/safety-logo.png') }}" class="product-footer-img"
+                    alt="Logo Dover">
+                <img src="{{ asset('assets/img/product/iso-logo.png') }}" class="product-footer-img" alt="Logo Dover">
             </div>
         </section>
     </main>
